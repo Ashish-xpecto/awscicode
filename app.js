091aa98ca7a15df2e01a44458cf03bf31a2f3697
Ashish-xpecto <ashish@work4clients.in>
2025-04-17 14:57:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
-
 const app = express();
 
-// Serve the HTML or send a response
 app.get('/', (req, res) => {
-  res.status(200).send("hello world from git ok");
+  res.status(200).send("hello world from git");
 });
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
