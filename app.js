c215ec07c0fe9da46545462e7880f26facdeff28
Ashish-xpecto <ashish@work4clients.in>
2025-04-17 14:43:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
