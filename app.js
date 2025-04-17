d5b700699f7276c303c7b5ac90d84c90507c07ab
Ashish-xpecto <ashish@work4clients.in>
2025-04-17 15:15:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.status(200).send("hello world from git");
 });
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
